61e9f0451e1c79645f4ec75b718543c417f6c2e7
Monise Jacheta <monisel.jacheta@gmail.com>
2024-12-06 09:09:52 -0300

    feat: added conde style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,8 +5,10 @@ const server = express();
 
 // Construindo rota GET
 // A rota GET recebe uma função que manipula a requisição (req) e a resposta (res)
+
+
 server.get('/', (_, res) => {
-    res.send('Olá Dev!'); // Retorna 'Olá Dev'
+  res.send('Olá Dev!'); // Retorna 'Olá Dev'
 });
 
 // Exporta a instância do servidor
