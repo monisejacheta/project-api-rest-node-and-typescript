597a851796e676c68e9b9e6f3fcc35aafab3b395
Monise Jacheta <monisel.jacheta@gmail.com>
2024-12-06 08:38:39 -0300

    feat: Initial config

---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// Cria a instância do servidor
+const server = express();
+
+// Construindo rota GET
+// A rota GET recebe uma função que manipula a requisição (req) e a resposta (res)
+server.get('/', (_, res) => {
+    res.send('Olá Dev!'); // Retorna 'Olá Dev'
+});
+
+// Exporta a instância do servidor
+export { server };
+
+
